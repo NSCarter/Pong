85cc055d81ff934cb6821e71ba7cb721ebc989a4
Niamh Carter <niamh.carter13@gmail.com>
2023-12-19 15:43:05 +0000

    Refactored paddle action code

--- Paddle.gd
+++ Paddle.gd
@@ -4,16 +4,13 @@ extends Node2D
 
 
 func _process(_delta):
-	if player == 1:
-		if Input.is_action_pressed("player_1_down"):
-			move_down()
-		if Input.is_action_pressed("player_1_up"):
-			move_up()
-	elif player == 2:
-		if Input.is_action_pressed("player_2_down"):
-			move_down()
-		if Input.is_action_pressed("player_2_up"):
-			move_up()
+	var action_up = "player_" + str(player) + "_up"
+	var action_down = "player_" + str(player) + "_down"
+
+	if Input.is_action_pressed(action_up):
+		move_up()
+	if Input.is_action_pressed(action_down):
+		move_down()
 
 func move_up():
 	position.y -= 5
